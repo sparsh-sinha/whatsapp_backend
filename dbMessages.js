cba8257f9c4d88ba5d62aba8df974d612f351fcd
sparshsinha <ss2709@srmist.edu.in>
2023-02-26 01:55:30 +0530

    new changes

--- dbMessages.js
+++ dbMessages.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const whatsappSchema =mongoose.Schema({
+const whatsappSchema = mongoose.Schema({
     message: String,
     name: String,
     timestamp: String
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import Messages from "./dbMessages.js";
 
 // importing 
 
@@ -8,11 +8,54 @@ import mongoose from "mongoose";
 const app = express()
 const port=process.env.PORT || 9000
 // middleware
+app.use(express.json())
 //db
 const connect_url='mongodb+srv://sparsh123:6cJMRRhgB5cqpB4f@cluster0.72fd7wj.mongodb.net/whatsappdb?retryWrites=true&w=majority'
 mongoose.connect(connect_url)
 //
 // api routes
 app.get('/',(req,res)=>res.status(200).send("hello"));
+
+app.get('/messages/sync',(req,res) => {
+
+    Messages.find((err, data) => {
+        if (err){
+            res.status(500).send(err)
+
+        }
+        else{
+            res.status(200).send(data)
+        }
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+app.post('/messages/new',(req,res) => {
+    const dbMessage = req.body
+
+    Messages.create(dbMessage, (err, data) => {
+        if (err){
+            res.status(500).send(err)
+
+        }
+        else{
+            res.status(201).send(data)
+        }
+    })
+})
+
+
+
+
 //listener
 app.listen(port,()=>console.log(`Listening on localhost:$({port}`));
